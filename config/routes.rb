110f67e13d98263c98bd7de0668733851ea06c59
Sim perso <leivaprogramming@gmail.com>
2021-09-09 15:51:28 -0400

    add rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  scope '(:locale)', locale: /fr|en/ do
-    root to: 'pages#home'
-
+  scope "(:locale)", locale: /fr|en/ do
+    root to: "pages#home"
   end
 end
